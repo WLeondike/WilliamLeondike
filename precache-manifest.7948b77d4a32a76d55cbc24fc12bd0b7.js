c6db15c108816f3e8c47a4be1b3ead091f20f58c
WLeondike <billyleondike@gmail.com>
2020-09-08 18:13:09 -0400

    Updates

---
+++ precache-manifest.7948b77d4a32a76d55cbc24fc12bd0b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3409236556d882ced8cc",
+    "url": "/WilliamLeondike/static/css/main.a4cd52ef.chunk.css"
+  },
+  {
+    "revision": "3409236556d882ced8cc",
+    "url": "/WilliamLeondike/static/js/main.34092365.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/WilliamLeondike/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "a94e7905b7b197635f52",
+    "url": "/WilliamLeondike/static/js/runtime~main.a94e7905.js"
+  },
+  {
+    "revision": "21d128b78a24270f07b04688ca6e6915",
+    "url": "/WilliamLeondike/index.html"
+  }
+];
